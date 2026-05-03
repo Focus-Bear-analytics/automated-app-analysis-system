940be63f9f1e18eec3bed9b775853931863e260e
Sreeraj <157796855+sreerajhere@users.noreply.github.com>
2026-05-03 13:11:14 +1000

    Added an improved sentiment analysis script
    Added a script to classify review sentiment and generate sentiment distribution output for analysis.

---
+++ scripts/improved_sentiment_analysis.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from textblob import TextBlob
+
+# Loading the existing reviews file
+df = pd.read_csv("data/curated/reviews_with_sentiment.csv")
+
+# Function to generate improved sentiment
+def get_sentiment(text):
+    try:
+        polarity = TextBlob(str(text)).sentiment.polarity
+        
+        if polarity > 0.2:
+            return "Positive"
+        elif polarity < -0.2:
+            return "Negative"
+        else:
+            return "Neutral"
+            
+    except:
+        return "Neutral"
+
+# Applying to see if new sentiment generates
+df["Improved_Sentiment"] = df["review"].apply(get_sentiment)
+
+print("Sentiment distribution:")
+print(df['sentiment_label'].value_counts())
+
+# Saving the new file
+df.to_csv("data/reviews_with_improved_sentiment.csv", index=False)
+
+print("Improved sentiment file created successfully!")
